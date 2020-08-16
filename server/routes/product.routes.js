82e048161c88bc6678ee7d764639584348b5225e
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-16 16:18:29 -0500

    updated all routes dealing with product model.

--- server/routes/product.routes.js
+++ server/routes/product.routes.js
@@ -2,5 +2,9 @@
 const ProductController = require('../controllers/product.controllers');
 
 module.exports = function(app){
-    app.post("/api/product", ProductController.create)
+    app.post("/api/product/new", ProductController.create);
+    app.get("/api/products/", ProductController.findAllProducts);
+    app.get("/api/products/:id", ProductController.findOne);
+    app.put("/api/products/update:id", ProductController.updateExistingProduct);
+    app.delete("/api/products/delete/:id");
 };
