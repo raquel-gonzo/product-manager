e11e9be5b7e41f89b2d7cf6d92e2456786b0c0a3
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-15 21:45:14 -0500

    first commit

---
+++ server/controllers/product.controllers.js
@@ -0,0 +1,20 @@
+const Product = require('./../models/product.models');
+const { create, update } = require('./../models/product.models');
+
+module.exports.index = (req, res) => {
+    index: (req, res) => {  // show all
+        
+    }
+    show: (req, res) => {   // show one
+
+    }
+    create: (req, res) => {
+
+    }
+    update: (req, res) => {
+
+    }
+    delete: (req, res) => {
+
+    }
+}
