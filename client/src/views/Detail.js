b77493c4553d5e23cdf2f9bb6e09a91857faf3be
raquel-gonzo <gonzalez.rachel.c@gmail.com>
2020-08-19 18:16:28 -0500

    deleting from detail view now navigates to main view on click.

--- client/src/views/Detail.js
+++ client/src/views/Detail.js
@@ -1,5 +1,5 @@
 import React,{useEffect, useState} from 'react'
-import {Link} from '@reach/router';
+import {Link, navigate} from '@reach/router';
 import axios from 'axios';
 
 const Detail = props => {
@@ -11,6 +11,15 @@ const Detail = props => {
                 ...res.data.product
             }))
     }, [props.id])
+        
+    const deleteProduct = (productId) =>{   // productId is the argument for the axios delete function. 
+        axios.delete(`http://localhost:8000/api/products/${productId}`)
+        .then(res => {
+            navigate('/')
+        })
+        .catch(err => console.log(err));
+    }
+
     return (
         <div>
             <p>Title: {product.title}</p>
@@ -19,6 +28,8 @@ const Detail = props => {
             <Link to="/">Back</Link>
             <br/>
             <Link to={"/" + product._id + "/edit"}>Edit</Link>
+            <br/>
+            <button onClick={(e) => {deleteProduct(product._id)}}>Delete</button>
         </div>
     )
 }
